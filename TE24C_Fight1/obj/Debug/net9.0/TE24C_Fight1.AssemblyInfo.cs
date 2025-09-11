90351d34bf42eac3faeee788bfd576482f5d0f86
Aaron Granath TE24C <divedconh287@gmail.com>
2025-09-11 08:37:58 +0200

    råkade lägga till ett till projekt i fel mapp

--- TE24C_Fight1/obj/Debug/net9.0/TE24C_Fight1.AssemblyInfo.cs
+++ TE24C_Fight1/obj/Debug/net9.0/TE24C_Fight1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TE24C_Fight1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7efc240593e0e225e532c53077dd946447e41c7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TE24C_Fight1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TE24C_Fight1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
